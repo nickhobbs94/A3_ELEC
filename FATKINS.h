4b940e715a9caff1778753381eb98739f5659e18
Nicholas Hobbs <c3184244@uon.edu.au>
2015-05-21 15:15:17 +1000

    Began constructing interface with old code

--- FATKINS.h
+++ FATKINS.h
@@ -5,58 +5,138 @@
 #include <stdio.h>
 #include "alt_types.h"
 
-struct masterBootRecord{
-	alt_8 bootCode[446];
-	alt_8 partitionTable1[16];
-	alt_8 partitionTable2[16];
-	alt_8 partitionTable3[16];
-	alt_8 partitionTable4[16];
-	alt_8 signature[2];
-};
+/* Special codes for file entries in directory sector */
+#define   END_OF_DIR         0x00
+#define   INIT_BYTE_IS_E5    0x05
+#define   DOT_ENTRY          0x2e
+#define   ENTRY_IS_ERASED    0xe5
 
-struct partitionTable{
-	alt_8 bootFlag;
-	alt_8 CHS_begin[3];
-	alt_8 typeCode;
-	alt_8 CHS_end[3];
-	alt_8 LBA_begin[4];
-	alt_8 Num_sectors[4];
-};
+/* File attributes bitmask */
+/* example usage: if (attribute & HIDDEN); */
+#define   READONLY       0b00000001
+#define   HIDDEN         0b00000010
+#define   SYSTEM         0b00000100
+#define   VOLUME_LABEL   0b00001000
+#define   SUBDIRECTORY   0b00010000
+#define   ARCHIVE        0b00100000
+#define   DEVICE         0b01000000
+#define   UNUSED         0b10000000
+#define   LONGFILENAME   0x0f
 
-struct bootSector{
-	alt_8 jumpInstruction[3];
-	alt_8 OEM_Name[8];
-	alt_8 bytesPerSector[2];
-	alt_8 sectorsPerCluster;
-	alt_8 numReservedSectors[2];
-	alt_8 numFileAllocTables;
-	alt_8 unused0[2];
-	alt_8 numSectors[2];  // Nonzero if < 2^16
-	alt_8 mediaDescriptor;
-	alt_8 logicalSectorsPerFAT[2]; // zero for fat32
-	alt_8 unused1[8];
-	alt_8 numSectorsExtra[4];  // num sectors > 2^16
-	alt_8 sectorsPerFAT[4];
-	alt_8 unused2[4];
-	alt_8 clusterNumOfRootDir[4]; // mostly 2
-	alt_8 unused3[42];
-	alt_8 OS_bootCode[420]; // blazeit
-	alt_8 signature[2];
-};
 
-struct FAT_infoSector{
-	alt_8 signature0[4];
-	alt_8 unused0[480];
-	alt_8 signature1[4];
-	alt_8 numFreeClusters[4];
-	alt_8 numMostRecentAllocCluster[4];
-	alt_8 unused1[14];
-	alt_8 signature2[2];
-};
+/* Long file name first byte (sequence number) */
+#define  DELETED_ENTRY     8
+#define  LAST_PART_LFN     4
+#define  OTHER_PART_LFN    0
 
-struct directory{
-	//alt_8
-};
+typedef struct masterBootRecord{
+	alt_u8 bootCode[446];
+	alt_u8 partitionTable1[16];
+	alt_u8 partitionTable2[16];
+	alt_u8 partitionTable3[16];
+	alt_u8 partitionTable4[16];
+	alt_u8 signature[2];
+} masterBootRecord;
+
+typedef struct partitionTable{
+	alt_u8 bootFlag;
+	alt_u8 CHS_begin[3];
+	alt_u8 typeCode;
+	alt_u8 CHS_end[3];
+	alt_u8 LBA_begin[4];
+	alt_u8 Num_sectors[4];
+} partitionTable;
+
+typedef struct bootSector{
+	alt_u8 jumpInstruction[3];
+	alt_u8 OEM_Name[8];
+	alt_u8 bytesPerSector[2];
+	alt_u8 sectorsPerCluster;
+	alt_u8 numReservedSectors[2];
+	alt_u8 numFileAllocTables;
+	alt_u8 unused0[2];
+	alt_u8 numSectors[2];  // Nonzero if < 2^16
+	alt_u8 mediaDescriptor;
+	alt_u8 logicalSectorsPerFAT[2]; // zero for fat32
+	alt_u8 unused1[8];
+	alt_u8 numSectorsExtra[4];  // num sectors > 2^16
+	alt_u8 sectorsPerFAT[4];
+	alt_u8 unused2[4];
+	alt_u8 clusterNumOfRootDir[4]; // mostly 2
+	alt_u8 unused3[42];
+	alt_u8 OS_bootCode[420]; // blazeit
+	alt_u8 signature[2];
+} bootSector;
+
+typedef struct FAT_infoSector{
+	alt_u8 signature0[4];
+	alt_u8 unused0[480];
+	alt_u8 signature1[4];
+	alt_u8 numFreeClusters[4];
+	alt_u8 numMostRecentAllocCluster[4];
+	alt_u8 unused1[14];
+	alt_u8 signature2[2];
+} FAT_infoSector;
+
+typedef struct directoryEntry{      // File entry in the sector of a directory
+	alt_u8 fileName[8];             // First byte can be special codes
+	alt_u8 fileExtenstion[3];
+	alt_u8 fileAttributes;
+	alt_u8 reserved;
+	alt_u8 ms_timeCreation;         // millisecond resolution of time creation
+	alt_u8 hms_timeCreation[2];     // hours minutes seconds of creation time
+	alt_u8 ymd_timeCreation[2];     // year month day of creation time
+	alt_u8 ymd_lastAccessDate[2];   // year month day of last access date
+	alt_u8 startCluster_msb[2];     // most significant bits of the start cluster
+	alt_u8 hms_lastModifiedDate[2]; // hours minutes seconds of last modify time
+	alt_u8 ymd_lastModifiedDate[2]; // year month day last modified date
+	alt_u8 startCluster_lsb[2];     // least significant bits of start cluster
+	alt_u8 fileSizeInBytes[4];
+} directoryEntry;
+
+
+/* --------------------------- extracted structures --------------------------- */
+
+
+/* Boot sector important info structure */
+typedef struct EmbeddedBootSector{
+	alt_32 num_reserved_sectors;
+	alt_32 startingSectorOfFAT;
+	alt_32 numOfFATs;
+	alt_32 sectorsPerFAT;
+	alt_32 firstClusterStart;
+	alt_32 sectorsPerCluster;
+	alt_32 rootClusterStart;
+} EmbeddedBootSector;
+
+/* Struct with everything important about a partition */
+typedef struct EmbeddedPartition{
+	alt_u8 typeCode; // is it FAT32?
+	alt_32 bootSectorAddress;
+	EmbeddedBootSector bootSector;
+} EmbeddedPartition;
+
+typedef struct FileSystemInfo{
+	EmbeddedPartition partition; // Sector address of current partition
+} FileSystemInfo;
+
+/* Struct with everything important about the file system */
+typedef struct EmbeddedFileSystem{
+	alt_u8 numberOfPartitions;
+	alt_32 addressesOfPartitions;
+	FileSystemInfo myFs;  // current partition
+} EmbeddedFileSystem;
+
+typedef struct File{
+	alt_u8 FileName[12]; // 8 (name) + 3 (extension) + 1 (null terminate)
+	alt_32 FileSize;
+	alt_u8 Attribute;
+} File;
+
+typedef struct DirList{
+	File currentEntry;
+} DirList;
+
 
 #endif
 
--- tester.c
+++ tester.c
@@ -2,15 +2,18 @@
 
 #include "FATKINS.h"
 #include "alt_types.h"
-#include "spi_sd.h"
+//#include "spi_sd.h"
 #include <stdio.h>
+#include "altstring.h"
 
 #define SECTOR_SIZE 512
 #define CLUSTERS_IN_FATSECTOR 128
 #define BYTES_PER_ENTRY 4
 
-/*alt_32 sd_readSector(alt_32 address, alt_8* buffer){
+/* Only when not using NIOS board */
+alt_32 sd_readSector(alt_32 address, alt_u8* buffer){
 	FILE* fp = fopen("/Users/nicholashobbs/Desktop/SD.dmg", "r");
+	//FILE* fp = fopen("/Users/nicholashobbs/Downloads/Torrent/2015-05-05-raspbian-wheezy.img", "r");
 	if (fp == NULL){
 		return -1;
 	}
@@ -19,7 +22,7 @@
 	fread(buffer, 1, 512, fp);
 	fclose(fp);
 	return 0;
-}*/
+}
 
 
 void printSector(alt_u8* buffer){
@@ -37,18 +40,6 @@ void printSector(alt_u8* buffer){
 }
 
 
-
-/*int main(void){
-	alt_8 buffer[512];
-	alt_32 result = sd_readSector(0, buffer);
-	if (result != 0){
-		printf("Unable to read sector\n");
-		return -1;
-	}
-	printSector(buffer);
-	return 0;
-}*/
-
 alt_32 extract_little(alt_u8* buffer, alt_32 size){
 	alt_32 sum = 0;
 	alt_32 i;
@@ -56,6 +47,25 @@ alt_32 extract_little(alt_u8* buffer, alt_32 size){
 		sum += buffer[i] << i*8;
 	}
 	return sum;
+}
+
+alt_32 getBootSector(alt_32 bootSectorAddress, EmbeddedBootSector* bootSectorPtr){
+	alt_u8 buffer[SECTOR_SIZE];
+
+	sd_readSector(bootSectorAddress, buffer);
+	//printSector(buffer);
+
+	bootSector* bootSec = (bootSector*) buffer;
+
+	bootSectorPtr->num_reserved_sectors = extract_little(bootSec->numReservedSectors,2);
+	bootSectorPtr->startingSectorOfFAT = bootSectorAddress + bootSectorPtr->num_reserved_sectors;
+	bootSectorPtr->numOfFATs = bootSec->numFileAllocTables;
+	bootSectorPtr->sectorsPerFAT = extract_little(bootSec->sectorsPerFAT,4);
+	bootSectorPtr->firstClusterStart = bootSectorPtr->startingSectorOfFAT + bootSectorPtr->numOfFATs * bootSectorPtr->sectorsPerFAT;
+	bootSectorPtr->sectorsPerCluster = bootSec->sectorsPerCluster;
+	bootSectorPtr->rootClusterStart = extract_little(bootSec->clusterNumOfRootDir, 4);
+
+	return 0;
 }
 
 alt_32 getFATentry(alt_32 clusterNum, alt_32 startingSectorOfFAT){
@@ -67,63 +77,59 @@ alt_32 getFATentry(alt_32 clusterNum, alt_32 startingSectorOfFAT){
 	return extract_little(FAT_Entry, BYTES_PER_ENTRY);
 }
 
-int main(void){
-	printf("%x\n", if_initInterface());
+alt_u8 countPartitions(alt_u8* buffer){
+	masterBootRecord* MasterBootRecord = (masterBootRecord*) buffer;
+	alt_u8 count = 0;
+	partitionTable* partition;
+
+	partition = (partitionTable*) MasterBootRecord->partitionTable1;
+	if (partition->typeCode) count++;
+	partition = (partitionTable*) MasterBootRecord->partitionTable2;
+	if (partition->typeCode) count++;
+	partition = (partitionTable*) MasterBootRecord->partitionTable3;
+	if (partition->typeCode) count++;
+	partition = (partitionTable*) MasterBootRecord->partitionTable4;
+	if (partition->typeCode) count++;
+
+	return count;
+
+}
+
+alt_32 efs_init_partitions(EmbeddedFileSystem* fileSystem){
 	alt_u8 buffer[SECTOR_SIZE];
-	struct masterBootRecord* MBR;
-	MBR = (struct masterBootRecord*) buffer;
+	masterBootRecord* MBR;
+	MBR = (masterBootRecord*) buffer;
 	sd_readSector(0, buffer);
-	printSector(buffer);
 
-	struct partitionTable* partition1;
-	partition1 = MBR->partitionTable1;
+	partitionTable* partition;
+	partition = (partitionTable*) MBR->partitionTable1;
 
-	printf("typecode: %x\n", partition1->typeCode);
-	printf("LBA_begin: %d\n", extract_little(partition1->LBA_begin,4));
-	printf("Num_sectors: %d\n", extract_little(partition1->Num_sectors,4));
+	fileSystem->numberOfPartitions = countPartitions(buffer);
+	return 0;
+}
 
-	/* For a single partition */
+alt_32 efs_init(EmbeddedFileSystem* fileSystem, alt_8 deviceName[]){
+	//printf("%x\n", if_initInterface());   // if on NIOS board uncomment this line
 
-	alt_32 bootSector = extract_little(partition1->LBA_begin,4);
-	alt_32 numSectors = extract_little(partition1->Num_sectors,4);
+	/* Load partition info into EmbeddedFileSystem struct */
+	efs_init_partitions(fileSystem);
 
-	sd_readSector(bootSector, buffer);
-	//printSector(buffer);
-
-	struct bootSector* bootSec = (struct bootSector*) buffer;
-
-	alt_32 num_reserved_sectors = extract_little(bootSec->numReservedSectors,2);
-	alt_32 startingSectorOfFAT = bootSector + num_reserved_sectors;
-	alt_32 numOfFATs = bootSec->numFileAllocTables;
-	alt_32 sectorsPerFAT = extract_little(bootSec->sectorsPerFAT,4);
-	alt_32 firstClusterStart = startingSectorOfFAT + numOfFATs * sectorsPerFAT;
-	alt_32 sectorsPerCluster = bootSec->sectorsPerCluster;
-	alt_32 rootClusterStart = extract_little(bootSec->clusterNumOfRootDir, 4);
-
-	printf("Num res sectors: %d\n", num_reserved_sectors);
-	printf("starting sector of FAT: %d\n", startingSectorOfFAT);
-	printf("numofFATs: %d\n", numOfFATs);
-	printf("sectorsPerFat: %d\n", sectorsPerFAT);
-	printf("firstClusterStart: %d\n", firstClusterStart);
-	printf("sectorsPerCluster: %d\n", sectorsPerCluster);
-	printf("rootClusterStart: %d\n", rootClusterStart);
-
-	/* for a cluster num */
-	alt_32 cluster_num = rootClusterStart;
-	alt_32 clusterFirstSector = firstClusterStart + sectorsPerCluster * (cluster_num-2);
-
-	//sd_readSector(clusterFirstSector, buffer);
-	//printSector(buffer);
-	alt_32 i;
-	for (i=0; i<1; i++){
-		sd_readSector(firstClusterStart+i, buffer);
-		printSector(buffer);
-	}
-	clusterFirstSector = firstClusterStart + sectorsPerCluster * (0x0101-2);
-	sd_readSector(clusterFirstSector, buffer);
-	printSector(buffer);
-
-	printf("%x\n",getFATentry(0x0101, startingSectorOfFAT));
+	/* TODO: check which device to mount and change partition depending on that (deviceName[]) */
+	/* for now, assume mounting first partition */
 
 	return 0;
 }
+
+int main(void){
+	EmbeddedFileSystem* efsl;
+	alt_32 check = efs_init(efsl, "/dev/sda");
+
+	if (check==0){
+		printf("Filesystem correctly initialized\n");
+	} else {
+		printf("Could not init filesystem\n");
+	}
+
+	return 0;
+}
+
